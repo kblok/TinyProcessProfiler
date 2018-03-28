ee14712b31830eb58484a872856811c3e3d4406e
Dario Kondratiuk <dariokondratiuk@gmail.com>
2018-03-28 09:15:36 -0300

    First tiny draft

---
+++ TinyProcessProfiler/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TinyProcessProfiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string process, int iterations) = GetArguments(args);
+        }
+
+        private static (string process, int iterations) GetArguments(string[] args)
+        {
+            var process = string.Empty;
+            var iterations = 1;
+
+            if(args.Length > 0)
+            {
+                process = args[0];
+            }
+            else
+            {
+                PrintArgumentErrorMessage();
+            }
+
+            if (args.Length > 1 && !int.TryParse(args[1], out iterations))
+            {
+                PrintArgumentErrorMessage();
+                process = string.Empty;
+            }
+
+            return (process, iterations);
+        }
+
+        private static void PrintArgumentErrorMessage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
